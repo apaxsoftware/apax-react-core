85b654d8e1ad2b5829427a5acc012993c16561b7
David Mitchell <dmitchek@gmail.com>
2020-02-20 13:12:14 -0500

    Added CLEAR_ERRORS action
    Added CLEAR_ERRORS action to clear any account errors in the store

--- src/actions/account.js
+++ src/actions/account.js
@@ -9,6 +9,7 @@ export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const LOGOUT = 'LOGOUT';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 export const setUserToken = (token) => {
   return {
